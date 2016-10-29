0efb4f905cd516166d5a4ff8cdd52fa84240c9cd
kostya <kostyatest@gmail.com>
2016-10-29 23:41:11 +0300

    first tutsplus yii2 commit

---
+++ frontend/views/site/say.php
@@ -0,0 +1,15 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Say';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Hello <?= "<b>".$message."<b>"?></p>
+
+</div>
